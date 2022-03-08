fd8a4e99befc9c55987054eae691c6251526b039
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-08 12:48:35 -0800

    Add files via upload
    Added instructions for assignment

---
+++ xhrPractice.js
@@ -0,0 +1,27 @@
+//first open the integrated terminal 
+//npm init
+//add "start": "nodemon"  to scripts
+
+//create a variable called XMLHttpRequest to require ("xhr");
+//create a variable called xhr and set it equal to a new XMLHttpRequest()
+
+//create a variable for url and set it equal to 'https://dog.ceo/api/breeds/list/all'
+
+var method = 'GET'; 
+
+//use xhr variable with open method
+//pass in methos and url as paramenters
+
+
+//when we make to connection, this is what the property will be
+//DONE means operation is completed and we've done the response
+
+//use xhr variable with onreadystatechange and set equal to anonymous function
+//create if/else if state for DONE XMLHttpRequest && 200 status
+//create parsedJSON varible for JSON.parse method
+//log beagle, elkhound, hound, papillon, and spaniel
+//use else/if to log error
+
+
+//use send 
+xhr.send();
