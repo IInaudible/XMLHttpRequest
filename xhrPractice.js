a13c0ea70d2e4ac10f28bc79a8a5b0a4fba6d194
michaelberish <mikeyberish@gmail.com>
2022-04-28 14:30:20 -0700

    done

--- xhrPractice.js
+++ xhrPractice.js
@@ -7,13 +7,20 @@
 //create a variable called XMLHttpRequest to require ("xhr");
 //create a variable called xhr and set it equal to a new XMLHttpRequest()
 
+let XMLHttpRequest = require('xhr2');
+
+let xhr = XMLHttpRequest;
+
 //create a variable for url and set it equal to 'https://dog.ceo/api/breeds/list/all'
 
-var method = 'GET'; 
+let url = 'https://dog.ceo/api/breeds/list/all';
+
+let method = 'GET'; 
 
 //use xhr variable with open method
 //pass in methos and url as paramenters
 
+xhr.open(method, url);
 
 //when we make to connection, this is what the property will be
 //DONE means operation is completed and we've done the response
@@ -24,6 +31,16 @@ var method = 'GET';
 //log beagle, elkhound, hound, papillon, and spaniel
 //use else/if to log error
 
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        // console.log(xhr.responseText)
+        let parsedJSON = JSON.parse(xhr.responseText).message;
+        console.log(parsedJSON);
+    } else if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) {
+        console.error(error);
+    }
+}
 
 //use send 
+
 xhr.send();
